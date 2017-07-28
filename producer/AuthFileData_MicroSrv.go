c818a83675103602699ae439a0f60b02230d04fc
Ernesto Rodriguez <aspacsa@gmail.com>
2017-07-28 10:21:04 -0400

    Clean up code and package each file record in a single message to be sent to broker.

--- producer/AuthFileData_MicroSrv.go
+++ producer/AuthFileData_MicroSrv.go
@@ -42,14 +42,12 @@ func init() {
 	multiple paths we are going to use concurrency.
 */
 func main() {
-	progName := os.Args[0]
-	logtypes.Info.Printf("Micro Service: %s\n", progName)
-	totArgs := len(os.Args)
+	logtypes.Info.Printf("Micro Service: %s\n", os.Args[0])
 	var fileName string
 	var brokers string
 	var topic string
 
-	if totArgs > 1 {
+	if len(os.Args) > 1 {
 		fileName = os.Args[1]
 		brokers = os.Args[2]
 		topic = os.Args[3]
@@ -57,22 +55,69 @@ func main() {
 			log.Fatalln("Must specify the name of file containing paths.")
 		}
 	} else {
-		log.Fatalln(fmt.Sprintf("Usage: %s <name paths file>", progName))
+		log.Fatalln(fmt.Sprintf("Usage: %s <name of file containing paths>, <broker1,broker2>, <topic>", os.Args[0]))
 	}
 
-	var paths []string
-	paths = read(fileName)
+	paths := read(fileName)
 
-	cpus := runtime.NumCPU()
-	runtime.GOMAXPROCS(cpus)
-	wg.Add(len(paths))
+	start(&paths, brokers, topic)
+
+	logtypes.Info.Println("Finished.")
+}
+
+/*
+	Here we launch a series of goroutines to process
+	all paths containing the targeted files to read from.
+*/
+func start(paths *[]string, brokers string, topic string) {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	wg.Add(len(*paths))
 	logtypes.Info.Println("Processing the following path(s):")
-	for _, path := range paths {
+	for _, path := range *paths {
 		go process(path, brokers, topic)
 	}
 	wg.Wait()
+}
 
-	logtypes.Info.Println("Finished.")
+/*
+	Here we determine if the path to file is valid to
+	avoid going to the wrong directory,
+	if valid then we read all files in directory
+	reading each line in each one of them.
+*/
+func process(spath string, brokers string, topic string) {
+	defer wg.Done()
+	logtypes.Info.Println(spath)
+	dir := path.Dir(spath)
+
+	if _, err := os.Stat(dir); err == nil {
+		files, _ := filepath.Glob(spath)
+		for _, file := range files {
+			for idx, line := range read(file) {
+				message := formatmsg(formatln(filepath.Base(file), idx+1, &line))
+				fmt.Println(*message)
+			}
+			//client.Send(brokers, topic, line)
+		}
+	} else {
+		logtypes.Error.Printf("Invalid path '%s'.\n", dir)
+	}
+}
+
+/*
+	Format each line read from the file.
+*/
+func formatln(filename string, idx int, line *string) *string {
+	newline := fmt.Sprintf("{\"File\":\"%s\", \"Row\":%s, \"Data\":\"%s\"}", filename, strconv.Itoa(idx), *line)
+	return &newline
+}
+
+/*
+	Package each record in final message format.
+*/
+func formatmsg(message *string) *string {
+	newmsg := fmt.Sprintf("{\"Record\": %s}", *message)
+	return &newmsg
 }
 
 /*
@@ -96,45 +141,9 @@ func read(fileName string) (lines []string) {
 	}
 
 	if err := scanner.Err(); err != nil {
-		logtypes.Error.Println("Error scanning file.")
+		logtypes.Error.Println("Error reading file.")
 	}
 	file.Close()
 
 	return mylines
-}
-
-/*
-	Here we determine if the path to file is valid to
-	avoid going to the wrong directory,
-	if valid then we read all files in directory
-	reading each line in each one of them.
-*/
-func process(spath string, brokers string, topic string) {
-	defer wg.Done()
-	logtypes.Info.Println(spath)
-	dir := path.Dir(spath)
-
-	if _, err := os.Stat(dir); err == nil {
-		files, _ := filepath.Glob(spath)
-		for _, file := range files {
-			//fname := filepath.Base(file)
-			var message string
-			for idx, line := range read(file) {
-				message += formatln(idx+1, &line)
-			}
-			message = formatmsg(filepath.Base(file), &message)
-			fmt.Println(message)
-			//client.Send(brokers, topic, line)
-		}
-	} else {
-		logtypes.Error.Printf("Invalid path '%s'.\n", dir)
-	}
-}
-
-func formatln(idx int, line *string) (newln string) {
-	return fmt.Sprintf(" {\"ID\":%s, \"Data\": \"%s\"}", strconv.Itoa(idx), *line)
-}
-
-func formatmsg(filename string, message *string) (newmsg string) {
-	return fmt.Sprintf("{\"%s\":[%s]}", filename, *message)
 }
